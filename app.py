26326f7e29e19071baaae31e2b0639e8220b9d20
Atlaz0 <sumholtimo@gmail.com>
2026-04-08 19:58:39 +0300

    better server + html line

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def App():
     return render_template("index.html", status=status, temp=temp)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000, debug="True")
